bcada880ea1e221a4411ceef4a12682eb488df3b
thaonguyen09 <thaonguyen_6491@yahoo.fr>
2017-04-20 01:24:58 +0200

    Vehicule : Ajouter véhicule
    Ajouter un véhicule vers la base SQL Server à partir d'une page HTML via
    Entity Framework

--- MasterMission.Website/Controllers/VehiculeController.cs
+++ MasterMission.Website/Controllers/VehiculeController.cs
@@ -34,40 +34,30 @@ namespace MasterMission.Controllers
             return item;
         }
 
-        /*
         // POST api/personne
         public Vehicule Post([FromBody] Vehicule value)
         {
-            var list = DataContext.GetListPersonne();
-            if(string.IsNullOrWhiteSpace(value.FirstName) || string.IsNullOrWhiteSpace(value.LastName))
+            using (var context = new MasterDbContext())
             {
-                throw new HttpException("Firstname and lastname can not be empty.");
+                var list = context.Vehicules;
+                list.Add(value);
+                context.SaveChanges();
+                return value;
             }
-            foreach (var item in list)
-            {
-                if (item.FirstName.ToLower().Equals(value.FirstName.ToLower()) || item.LastName.ToLower().Equals(value.LastName.ToLower()))
-                {
-                    throw new HttpException("A person with the same firstname and lastname exists already.");
-                }
-            }
-            value.Id = list.Count() + 1;
-            list.Add(value);
-            return value;
         }
 
         // PUT api/personne/5
         public void Put(int id, [FromBody] Vehicule value)
         {
-            var list = DataContext.GetListPersonne();
+            var list = (new MasterDbContext()).Vehicules;
             var item = list.FirstOrDefault(t => t.Id == id);
             if (item != null)
             {
-                item.FirstName = value.FirstName;
-                item.LastName = value.LastName;
-                item.Birthday = value.Birthday;
+                item.PlaqueMineralogique = value.PlaqueMineralogique;
+                item.PuissanceFiscale = value.PuissanceFiscale;
+                item.Kilometrage = value.Kilometrage;
             }
         }
-        */
 
         // DELETE api/personne/5
         public void Delete(int id)
